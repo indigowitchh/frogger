5db119fea12f7f84bb5a88ddfa78da2cefeb3e8d
rey <114239691+indigowitchh@users.noreply.github.com>
2023-03-16 09:47:44 -0600

    Create frog.h

---
+++ frog.h
@@ -0,0 +1,24 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<iostream>
+using namespace std; 
+
+class frog{
+private:
+	float xpos;
+	float ypos;
+	int lives;
+	float xVel;
+	float yVel;
+
+public:
+	frog();
+	void draw(sf::RenderWindow& window);
+	void jump(bool* keys);
+	float returnX() { return xpos; }
+	float returnY() { return ypos; }
+	void ded();
+	int returnLives() { return lives; }
+	void printInfo() { cout<<"I am a frog and my position is " << xpos << "," << ypos << endl; }
+};
+
