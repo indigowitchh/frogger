005e9e7b9aa573857a5b92d31231ce27a0c28c55
rey <114239691+indigowitchh@users.noreply.github.com>
2023-03-22 09:47:46 -0600

    Create Car.h

---
+++ Car.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<SFML/Graphics.hpp>
+#include<iostream>
+#include"globals.h"
+
+class Car {
+private:
+	float xpos;
+	float ypos;
+	int green = rand()%101+100;
+	int blue = rand()%101+100;
+	int direction = RIGHT;
+
+public:
+
+};
