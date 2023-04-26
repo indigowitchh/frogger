5d69c23dd2c1a2ca7df691ffb8d3ebd8a04986a9
rey <114239691+indigowitchh@users.noreply.github.com>
2023-04-26 09:33:45 -0600

    Update Car.h

--- Car.h
+++ Car.h
@@ -2,15 +2,19 @@
 #include<SFML/Graphics.hpp>
 #include<iostream>
 #include"globals.h"
+using namespace std;
 
 class Car {
 private:
-	float xpos;
-	float ypos;
-	int green = rand()%101+100;
-	int blue = rand()%101+100;
+	float xpos = 900;
+	float ypos = 500;
+	int green = rand() % 101 + 100;
+	int blue = rand() % 101 + 100;
 	int direction = RIGHT;
 
 public:
-
+	void draw(sf::RenderWindow& window);
+	void move();
+	bool collide(int x, int y);
+	void PrintInfo() { cout << "I am a car and my position is" << xpos << "," << ypos << endl; }
 };
